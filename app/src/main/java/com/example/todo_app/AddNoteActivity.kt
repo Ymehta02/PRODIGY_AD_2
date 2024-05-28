c8e31300096fc0d02e8cb69ce45c5bdc6ba40613
Ymehta02 <mehtayashvi909@gmail.com>
2024-05-29 00:25:10 +0530

    First commit

--- app/src/main/java/com/example/todo_app/AddNoteActivity.kt
+++ app/src/main/java/com/example/todo_app/AddNoteActivity.kt
@@ -1,28 +1,33 @@
 package com.example.todo_app
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.todo_app.databinding.ActivityMainBinding
+import com.example.todo_app.databinding.ActivityAddNoteBinding
 
-class MainActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NotesDatabaseHelper(this)
 
-        binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddNoteActivity::class.java)
-            startActivity(intent)
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0,title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
         }
 
 
---
+++ app/src/main/java/com/example/todo_app/Note.kt
@@ -0,0 +1,3 @@
+package com.example.todo_app
+
+data class Note(val id: Int, val title: String, val content: String )
